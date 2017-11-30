aeb0c16bded0bac0533aa7f068c2f8805abd80dc
Greg Bisbjerg <gbisbjerg@gmail.com>
2017-11-30 13:54:53 +0100

    Order and Customer class

---
+++ Gunner OrderList/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gunner_OrderList
+{
+    class Customer
+    {
+        private string _name;
+        private string _email;
+        private string _phoneNumber;
+        private string _address;
+        private string _blank;
+
+        public Customer()
+        {
+            
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public string Address { get => _address; set => _address = value; }
+        public string Blank { get => _blank; set => _blank = value; }
+    }
+}
---
+++ Gunner OrderList/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gunner_OrderList
+{
+    class Order
+    {
+        private string _name;
+        private string _email;
+        private string _phoneNumber;
+        private string _address;
+        private string _blank;
+        private string _blank2;
+        private string _description;
+
+        public Order()
+        {
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+        public string Address { get => _address; set => _address = value; }
+        public string Blank { get => _blank; set => _blank = value; }
+        public string Blank2 { get => _blank2; set => _blank2 = value; }
+        public string Description { get => _description; set => _description = value; }
+    }
+}
