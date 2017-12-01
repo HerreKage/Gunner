94fcd04869364dcc728cd53b17ca916044e44d52
DST001 <davidt72@yahoo.dk>
2017-12-01 10:39:08 +0100

    new class

---
+++ Gunner OrderList/ViewModelOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gunner_OrderList
+{
+    class ViewModelOrder : 
+    {
+        // Instance Field starts
+
+        private Order _newOrder;
+
+        // Instance Field Ends
+
+        // Constructor starts
+        public ViewModelOrder(Order o)
+        {
+            _newOrder= o;
+        }
+
+        // Constructor ends
+
+        public string Description
+        {
+            get
+            {
+                return _newOrder.Description;_}
+        }
+    }
+}
