7335f88f39bfa3a57f580dc63a2c31b27a17b8fc
Ahsan Javaid Iqbal <ahsa0005@edu.easj.dk>
2017-12-01 13:02:18 +0100

    DeleteCommand Mistake

---
+++ Gunner OrderList/Viewmodel/DeleteCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace Gunner_OrderList.Viewmodel
+{
+    class DeleteCommand :ICommand
+    {
+        private Catalog _catalog;
+
+        private Customer _customer;
+
+
+
+        public DeleteCommand(Catalog catalog, Customer customer);
+
+
+        {
+            _catalog = catalog;
+            _customer = customer;
+        }
+
+
+
+
+
+
+
+    public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
