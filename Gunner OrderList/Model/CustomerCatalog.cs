23dafce661e59e68236324ec7d084cc1e0b0e31d
Ahsan Javaid Iqbal <ahsa0005@edu.easj.dk>
2017-12-05 10:54:22 +0100

    Deletetion

--- Gunner OrderList/Model/CustomerCatalog.cs
+++ Gunner OrderList/Model/CustomerCatalog.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Gunner_OrderList.Viewmodel;
 
 namespace Gunner_OrderList
 {
     class CustomerCatalog
     {
         ObservableCollection<Customer> _customers;  //List of customers
-        private Customer _newCustomer = new Customer();
+        private Customer _selectedCustomer = new Customer();
 
         private static CustomerCatalog instance=null;
+        private CustomerCatalog _catalog;
+        private DeleteCommand _deletecommand;
+        
 
         private CustomerCatalog()
         {
             _customers = new ObservableCollection<Customer>();
+            _catalog=new CustomerCatalog();
+            _deletecommand= new DeleteCommand(_catalog, this );
         }
 
         public static CustomerCatalog Instance
@@ -36,5 +43,21 @@ namespace Gunner_OrderList
             get { return _customers; }
             set { _customers = value; }
         }
+
+
+
+
+        public ICommand DeletionCommand
+        {
+            get { return _deletecommand; }
+        }
+
+
+        public void delete()
+        {
+            _customers.Remove(_selectedCustomer);
+        }
+
+
     }
 }
