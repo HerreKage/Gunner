51c65457425dcf3df2da4de29d8ef8b5450d77f9
Jens Stanny Olafsson <jens703i@edu.easj.dk>
2017-12-08 11:00:48 +0100

    ^^

--- Gunner OrderList/View/LoginPage.xaml.cs
+++ Gunner OrderList/View/LoginPage.xaml.cs
@@ -20,11 +20,8 @@ namespace Gunner_OrderList.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class LoginPage : Page
     {
-        public BlankPage1()
-        {
-            this.InitializeComponent();
-        }
+        public LoginPage() => this.InitializeComponent();
     }
 }
