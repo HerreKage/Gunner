53c2c440ca6209e0d9dbce60beafd70ff909d3bc
Ahsan Javaid Iqbal <ahsa0005@edu.easj.dk>
2017-12-08 10:34:07 +0100

    Something old

--- Gunner OrderList/Model/CustomerCatalog.cs
+++ Gunner OrderList/Model/CustomerCatalog.cs
@@ -15,31 +15,34 @@ namespace Gunner_OrderList
 
         private static CustomerCatalog _instance=null;
 
+        private static int _customerNumber;
+        
         private CustomerCatalog()
         {
             _customers = new ObservableCollection<Customer>(); //Should load from database later
 
+            _customerNumber = 2;
 
-            Customer customer1 = new Customer();
-            customer1.CompanyNumber = "Apple";
-            customer1.Name = "David";
-            customer1.Address = "Birkemosvej 8";
-            customer1.PhoneNumber = "50652415";
-            customer1.CompanyNumber = "21-23-65-23";
-            customer1.Email = "apple@gmail.com";
+            //Customer customer1 = new Customer();
+            //customer1.CompanyNumber = "Apple";
+            //customer1.Name = "David";
+            //customer1.Address = "Birkemosvej 8";
+            //customer1.PhoneNumber = "50652415";
+            //customer1.CompanyNumber = "21-23-65-23";
+            //customer1.Email = "apple@gmail.com";
 
 
 
-            Customer customer2 = new Customer();
-            customer2.Company = "Ro's Kabab";
-            customer2.Name = "Greg";
-            customer2.Address = "Roskilde 8";
-            customer2.PhoneNumber = "80122545";
-            customer2.CompanyNumber = "43-98-75-23";
-            customer2.Email = "rokabab@gmail.com";
+            //Customer customer2 = new Customer();
+            //customer2.Company = "Ro's Kabab";
+            //customer2.Name = "Greg";
+            //customer2.Address = "Roskilde 8";
+            //customer2.PhoneNumber = "80122545";
+            //customer2.CompanyNumber = "43-98-75-23";
+            //customer2.Email = "rokabab@gmail.com";
 
-            _customers.Add(customer1);
-            _customers.Add(customer2);
+            //_customers.Add(customer1);
+            //_customers.Add(customer2);
         }
 
         public static CustomerCatalog Instance
