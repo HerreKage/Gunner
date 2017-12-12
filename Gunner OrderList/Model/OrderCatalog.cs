dad62cb332e79d07a5df291cb3d6f72ba29ecf2d
Jens Stanny Olafsson <jens703i@edu.easj.dk>
2017-12-12 12:40:13 +0100

    ^^

--- Gunner OrderList/Model/OrderCatalog.cs
+++ Gunner OrderList/Model/OrderCatalog.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Windows.UI.ViewManagement;
-using Persistency.Interfaces;
+﻿using System.Collections.ObjectModel;
 
 namespace Gunner_OrderList
 {
